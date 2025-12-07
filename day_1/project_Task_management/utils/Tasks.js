157c465ff21944a77b499ddd46af702695e5ea9c
DanielSaidov123 <ddny3622@gmail.com>
2025-12-07 16:51:32 +0200

    add function add_Task_to_arr and Deletion_Task_id in folder utils/Tasks

--- day_1/project_Task_management/utils/Tasks.js
+++ day_1/project_Task_management/utils/Tasks.js
@@ -6,11 +6,50 @@ function create_Task(name, details, status) {
     name,
     details,
     status,
-    Date:new Date()
+    Date: new Date(),
   };
 }
 
-function create_arr_Tasks( ){
-    return []
- } 
- 
+function create_arr_Tasks() {
+  return [];
+}
+
+function add_Task_to_arr(object_Task, arr_Tasks) {
+  arr_Tasks.push(object_Task);
+}
+
+function Deletion_Task_id(id_Task, arr_Tasks) {
+  let digit = false;
+  for (let i = 0; i < arr_Tasks.length; i++) {
+    if (arr_Tasks[i].id == id_Task) {
+      arr_Tasks.splise(i, 1);
+      digit = true;
+    }
+    if (digit) {
+      console.log("is deletion");
+    } else {
+      console.log(`is not deletion`);
+    }
+  }
+}
+
+
+
+
+
+
+
+// function init() {
+//   const arr_Tasks = create_arr_Tasks();
+//   const obj_1 = create_Task("daniel", "few", "fwe");
+//   const obj_2 = create_Task("daniel", "few", "fwe");
+//   const obj_3 = create_Task("daniel", "few", "fwe");
+
+//   add_Task_to_arr(obj_1, arr_Tasks);
+//   add_Task_to_arr(obj_2, arr_Tasks);
+//   add_Task_to_arr(obj_3, arr_Tasks);
+
+//   console.log(arr_Tasks);
+// }
+
+// init();

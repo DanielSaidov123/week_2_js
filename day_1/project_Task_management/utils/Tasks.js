b297d4716f7c1bf7a222c52c0ed0b642f50cb6f1
DanielSaidov123 <ddny3622@gmail.com>
2025-12-07 16:22:06 +0200

    add function create_Task

--- day_1/project_Task_management/utils/Tasks.js
+++ day_1/project_Task_management/utils/Tasks.js
@@ -1,3 +1,11 @@
-function create_Task(name,details,status){
-    
- } 
+import { nanoid } from "nanoid";
+
+function create_Task(name, details, status) {
+  return {
+    id: nanoid(),
+    name,
+    details,
+    status,
+    Date:new Date()
+  };
+}
